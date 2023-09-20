c3208a693ac090ff69898d40ee9712d4472245f8
Daus Carmichael <daviscarmichael@Dauss-MBP.lan>
2023-09-20 13:49:20 -0700

    website looks good starting work on python side

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+import os
+import sys
+import subprocess
+from os.path import exists
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -10,6 +14,17 @@ def index():
 def submit():
     # Call the C++ executable with 'entry' as input
     # Process the result and return it
+    letters = request.form['a']
+    letters += request.form['b']
+    letters += request.form['c']
+    letters = request.form['d']
+    letters += request.form['e']
+    letters += request.form['f']
+
+    center = request.form['i']
+    cmd=f'./main.out {letters} {center} 1'
+    result = subprocess.check_output(cmd, shell=True)
+    print(result)
     return "Result from C++: ..."
 
 if __name__ == '__main__':
